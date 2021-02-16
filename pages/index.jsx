90f4679d54fa34d588b62d1e62dcf1fe7a40f826
Nath Souza <nathallyccd@gmail.com>
2021-02-16 15:10:15 -0300

    Next init

---
+++ pages/index.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+
+const Home = () => {
+    return(
+        <div>
+            <h1>Hello world</h1>
+            <Counter />
+        </div>
+    )
+}
+
+const Counter = () =>{
+    const [count, setCount] = useState(0);
+
+    function add(){
+        setCount(count+1)
+    }
+
+    return(
+        <div>
+            <p>{count}</p>
+            <button onClick={add}>Adicionar</button>
+        </div>
+    )
+
+
+}
+
+export default Home;
