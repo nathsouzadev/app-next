1c0a067437c60908c9fd5c0e64aeeb6939f20598
Nath Souza <nathallyccd@gmail.com>
2021-03-14 13:44:07 -0300

    env

--- pages/api/time.jsx
+++ pages/api/time.jsx
@@ -1,8 +1,14 @@
-const time = (req, res) => {
+async function time (req, res) {
+    const apiSecret = process.env.POKE_API;
     const dynamicDate = new Date();
+
+    const pokeAPI = await fetch(`${apiSecret}`);
+    const pokeApiJSON = await pokeAPI.json();
+    const totalPokemon = pokeApiJSON.count;
 
     res.json({
         date: dynamicDate.toGMTString(),
+        pokemons: totalPokemon
     })
 }
 
